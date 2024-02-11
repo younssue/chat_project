de6f3ba6c9402036d18284a41f87bcbd86ce1675
younssue <love_rvd7@naver.com>
2024-02-11 18:36:04 +0900

    :tada: chat project start

---
+++ src/test/java/org/chat/chat_project/ChatProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chat.chat_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
